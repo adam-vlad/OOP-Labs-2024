34a76956683f5942158618269e271ce6c5078a8e
Adam Vlad <153214716+adam-vlad@users.noreply.github.com>
2024-03-10 20:27:51 +0200

    Delete Lab2/2_Studenti directory

--- Lab2/2_Studenti/functii.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "student.h"
-
-int ComparaNume(Student student1, Student student2);
-int ComparaNotaMate(Student student1, Student student2);
-int ComparaNotaEng(Student student1, Student student2);
-int ComparaNotaIst(Student student1, Student student2);
-int ComparaMedie(Student student1, Student student2);
