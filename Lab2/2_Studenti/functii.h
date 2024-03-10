697c80a87b69b66d2efb3893536d973c87df203f
Adam Vlad <vld.gaby@yahoo.com>
2024-03-10 20:26:17 +0200

    student

---
+++ Lab2/2_Studenti/functii.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "student.h"
+
+int ComparaNume(Student student1, Student student2);
+int ComparaNotaMate(Student student1, Student student2);
+int ComparaNotaEng(Student student1, Student student2);
+int ComparaNotaIst(Student student1, Student student2);
+int ComparaMedie(Student student1, Student student2);
